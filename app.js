964e8c77f01451f18172dd6d7ef3eff014bdf8b2
Zayed M Uddin <zayed.md.uddin273@gmail.com>
2024-05-09 09:44:50 -0400

    added the autoplay feature

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const paperImage = document.querySelector(".paper_img");
 const scissorImage = document.querySelector(".scissor_img");
 const poppedContent = document.querySelector(".popped_content");
 const resetBtn = document.querySelector(".btn_reset");
+const autoPlayBtn = document.querySelector(".btn_autoplay");
 
 let num_of_wins = parseInt(document.querySelector(".number_of_wins").innerText);
 let num_of_losses = parseInt(
@@ -17,6 +18,8 @@ const displayImg = document.createElement("img");
 const duplicateImg = document.createElement("img");
 
 let number_of_image = 0;
+let isAutoPlaying = false;
+let intervalID;
 
 const displayResult = function (your_selection, number_of_image) {
 	displayImg.src = `./images/emoji-${your_selection}.png`;
@@ -71,57 +74,88 @@ const resetGame = () => {
 	document.querySelector(".number_of_ties").innerText = num_of_ties;
 };
 
-rockImage.addEventListener("click", () => {
+const RockImage = function (your_selection) {
 	number_of_image = Math.trunc(Math.random() * 3) + 1;
 
 	if (number_of_image == 2) {
-		tiePopUp(2, number_of_image);
+		tiePopUp(your_selection, number_of_image);
 		num_of_ties++;
 		document.querySelector(".number_of_ties").innerText = num_of_ties;
 	} else if (number_of_image == 1) {
-		loserPopUp(2, number_of_image);
+		loserPopUp(your_selection, number_of_image);
 		num_of_losses++;
 		document.querySelector(".number_of_losses").innerText = num_of_losses;
 	} else if (number_of_image == 3) {
-		winnerPopUp(2, number_of_image);
+		winnerPopUp(your_selection, number_of_image);
 		num_of_wins++;
 		document.querySelector(".number_of_wins").innerText = num_of_wins;
 	}
-});
+};
 
-paperImage.addEventListener("click", () => {
+const PaperImage = function (your_selection) {
 	number_of_image = Math.trunc(Math.random() * 3) + 1;
 
 	if (number_of_image == 1) {
+		tiePopUp(your_selection, number_of_image);
 		num_of_ties++;
-		tiePopUp(1, number_of_image);
 		document.querySelector(".number_of_ties").innerText = num_of_ties;
 	} else if (number_of_image == 2) {
-		winnerPopUp(1, number_of_image);
+		winnerPopUp(your_selection, number_of_image);
 		num_of_wins++;
 		document.querySelector(".number_of_wins").innerText = num_of_wins;
 	} else if (number_of_image == 3) {
-		loserPopUp(1, number_of_image);
+		loserPopUp(your_selection, number_of_image);
 		num_of_losses++;
 		document.querySelector(".number_of_losses").innerText = num_of_losses;
 	}
-});
+};
 
-scissorImage.addEventListener("click", () => {
+const ScissorImage = function (your_selection) {
 	number_of_image = Math.trunc(Math.random() * 3) + 1;
 
 	if (number_of_image == 3) {
+		tiePopUp(your_selection, number_of_image);
 		num_of_ties++;
-		tiePopUp(3, number_of_image);
 		document.querySelector(".number_of_ties").innerText = num_of_ties;
 	} else if (number_of_image == 2) {
-		loserPopUp(3, number_of_image);
+		loserPopUp(your_selection, number_of_image);
 		num_of_losses++;
 		document.querySelector(".number_of_losses").innerText = num_of_losses;
 	} else if (number_of_image == 1) {
-		winnerPopUp(3, number_of_image);
+		winnerPopUp(your_selection, number_of_image);
 		num_of_wins++;
 		document.querySelector(".number_of_wins").innerText = num_of_wins;
+	}
+};
+
+const initGame = () => {
+	const your_selection = Math.trunc(Math.random() * 3) + 1;
+	if (your_selection == 3) {
+		ScissorImage(your_selection);
+	} else if (your_selection == 2) {
+		RockImage(your_selection);
+	} else if (your_selection == 1) {
+		PaperImage(your_selection);
+	}
+};
+
+const autoplay = function () {
+	initGame();
+};
+
+scissorImage.addEventListener("click", ScissorImage);
+rockImage.addEventListener("click", RockImage);
+paperImage.addEventListener("click", PaperImage);
+
+autoPlayBtn.addEventListener("click", function () {
+	if (!isAutoPlaying) {
+		autoPlayBtn.innerText = "Stop Auto Play";
+		intervalID = setInterval(autoplay, 1000);
+		isAutoPlaying = true;
+	} else {
+		autoPlayBtn.innerText = "Start Auto Play";
+		clearInterval(intervalID);
+		isAutoPlaying = false;
 	}
 });
 
